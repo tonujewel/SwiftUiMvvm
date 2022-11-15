d0c0715bd0a7fd9cdbbdb5d7f566ab4d9b3ba25b
Jewel rana <tonujewel@gmail.com>
2022-11-15 11:31:18 +0600

    Initial Commit

---
+++ SwiftUiMvvm/SwiftUiMvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiMvvmApp.swift
+//  SwiftUiMvvm
+//
+//  Created by Singularity - Jewel Rana on 15/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiMvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
